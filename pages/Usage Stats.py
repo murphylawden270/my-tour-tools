28758ecd0766034197df01e343c27de633ec15f6
Murphy Lawden <murphylawden270@gmail.com>
2026-08-02 20:52:36 +0530

    Enhance Usage Stats application with formatting updates
    Updated the application to include a shoutout to Eo Ut Mortus and improved formatting in the output.

--- pages/Usage Stats.py
+++ pages/Usage Stats.py
@@ -27,7 +27,7 @@ st.markdown(
 )
 
 st.title("Usage Stats Generator:")
-st.caption("This application uses [eo.herokuapp](https://replaystats-eo.herokuapp.com/) as a base.")
+st.caption("This application uses [eo.herokuapp](https://replaystats-eo.herokuapp.com/) as a base. HUGE S/O to [Eo Ut Mortus](http://www.smogon.com/forums/members/eo-ut-mortus.9718/) for this awesome tool.")
 
 options = Options()
 options.add_argument("--headless=new")
