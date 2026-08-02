30196c70a545c6e8664dcaae15ac409b0a80c9cd
Murphy Lawden <murphylawden270@gmail.com>
2026-08-02 20:01:59 +0530

    Refactor Usage Stats.py for improved readability

--- pages/Usage Stats.py
+++ pages/Usage Stats.py
@@ -31,8 +31,6 @@ st.title("Usage Stats Generator")
 options = Options()
 options.add_argument("--headless=new")
 options.add_argument("--disable-gpu")
-options.add_argument("--disable-dev-shm-usage") 
-options.add_argument("--no-sandbox") 
 
 bbcode = []
 
