410822d42ae97f9c77acebf979d0e3fd0a50a57d
Murphy Lawden <murphylawden270@gmail.com>
2026-08-02 20:00:29 +0530

    Enhance Usage Stats script with additional options
    Added options for headless Chrome and improved code structure.

--- pages/Usage Stats.py
+++ pages/Usage Stats.py
@@ -31,6 +31,8 @@ st.title("Usage Stats Generator")
 options = Options()
 options.add_argument("--headless=new")
 options.add_argument("--disable-gpu")
+options.add_argument("--disable-dev-shm-usage") 
+options.add_argument("--no-sandbox") 
 
 bbcode = []
 
