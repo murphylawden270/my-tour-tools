f0b3f5f7cf6dc17d854d544aab8646d38c056452
Murphy Lawden <murphylawden270@gmail.com>
2026-08-04 13:34:57 +0530

    Refactor Usage Stats.py for improved structure

--- pages/Usage Stats.py
+++ pages/Usage Stats.py
@@ -176,6 +176,7 @@ if st.session_state.final:
     done = "\n".join(st.session_state.final)
     st.caption(f"Processed {st.session_state.processed_formats} formats and {st.session_state.processed_replays} replays.")
     end = time.time()
-    st.caption(f"Time taken: {end - start} seconds.")
+    if start:
+        st.caption(f"Time taken: {end - start} seconds.")
     st.caption("BB Code:")
     st.code(done, language=None, height=300)
