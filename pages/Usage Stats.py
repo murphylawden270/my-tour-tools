2fc9984b6a3b3ffc4bd1dd73b7354ad1ba02d960
Murphy Lawden <murphylawden270@gmail.com>
2026-08-04 12:18:10 +0530

    Refactor Usage Stats.py to improve structure

--- pages/Usage Stats.py
+++ pages/Usage Stats.py
@@ -115,8 +115,7 @@ def replay(key, values):
     bbcode.append(Final)
     bbcode.append("")
 
-    st.session_state.all_bbcode[key] = bbcode
-    # return key, bbcode
+    return key, bbcode
 
 if st.button("Add Format", icon="➕"):
     name_dialog()
@@ -136,8 +135,8 @@ with col1:
             with ThreadPoolExecutor(max_workers=4) as executor:
                 output = list(executor.map(replay, st.session_state.project.keys(), st.session_state.project.values()))
 
-            # for key, bbcode in output:
-            #     st.session_state.all_bbcode[key] = bbcode
+            for key, bbcode in output:
+                st.session_state.all_bbcode[key] = bbcode
 
 with col2:
     if st.button("Clear All", use_container_width=True):
