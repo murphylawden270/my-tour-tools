be905c63e01658a16bfc61b9d8567a79d81b0833
Murphy Lawden <murphylawden270@gmail.com>
2026-09-03 13:57:53 +0530

    Refactor Usage Stats tool with Selenium integration

--- pages/Usage Stats.py
+++ pages/Usage Stats.py
@@ -1,16 +1,37 @@
+import time
+
+start = time.time()
+
 import streamlit as st
-import requests
 import re
-import collections
-import sys
-import threading
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
 from concurrent.futures import ThreadPoolExecutor
-import itertools
 
 st.set_page_config(
-    page_title="Tera Fetcher",
+    page_title="Usage Stats",
     layout="wide"
 )
+
+if "project" not in st.session_state:
+    st.session_state.project = {}
+
+if "all_bbcode" not in st.session_state:
+    st.session_state.all_bbcode = {}
+
+if "final" not in st.session_state:
+    st.session_state.final = []
+
+if "processed_replays" not in st.session_state:
+    st.session_state.processed_replays = 0
+
+if "processed_formats" not in st.session_state:
+    st.session_state.processed_formats = 0
 
 st.markdown(
     """
@@ -19,175 +40,141 @@ st.markdown(
             padding-top: 1.4rem;
             padding-bottom: 1rem;
         }
-    </style>
     """,
     unsafe_allow_html=True,
 )
 
-st.title("Tera Fetcher Tool For Usage Stats:")
+st.title("Usage Stats Generator:")
+st.caption("This application uses [eo.herokuapp](https://replaystats-eo.herokuapp.com/) as a base. HUGE S/O to [Eo Ut Mortus](http://www.smogon.com/forums/members/eo-ut-mortus.9718/) for this awesome tool.")
 
-if "replays" not in st.session_state:
-    st.session_state.replays = ""
+options = Options()
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
 
-if "final_bbcode" not in st.session_state:
-    st.session_state.final_bbcode = ""
+@st.dialog("Format Name")
+def name_dialog():
+    name = st.text_input("Enter Your Format Name:")
+    if st.button("Confirm"):
+        if name:
+            if name in st.session_state.project:
+                st.error("Format name already exists! Please choose another format name.")
+            else:
+                st.session_state.project[name] = []
+                st.rerun()
 
-if "table" not in st.session_state:
-    st.session_state.table = []
+def replay(key, values):
+    rl = len(values.splitlines())
+    bbcode = []
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://replaystats-eo.herokuapp.com/')
 
-if "no_tera" not in st.session_state:
-    st.session_state.no_tera = 0
+    Replay_urls = driver.find_element(By.NAME, "replay_urls")
+    driver.execute_script("arguments[0].value = arguments[1];", Replay_urls, values)
+    driver.execute_script("arguments[0].dispatchEvent(new Event('input', { bubbles: true }));", Replay_urls)
+    Submit = driver.find_element(By.NAME, "link_submit")
+    Submit.click()
 
-if "tera" not in st.session_state:
-    st.session_state.tera = {}
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "textarea.rawtext.results")))
 
-if "sorted_tera" not in st.session_state:
-    st.session_state.sorted_tera = {}
+    main = []
+    usage = []
+    url = []
+    TextArea = driver.find_elements(By.CSS_SELECTOR, "textarea.rawtext.results")
+    for i in TextArea:
+            usage.append(i.get_attribute("value"))
+        
+    for i, j in enumerate(usage):
+            if i == 0:
+                if "Leads" in usage[3] and "1" in usage[3]:
+                    a = j.replace("[CODE]", '''[URL='1']Moves and Teammates[/URL] | [URL='2']Combos[/url] | [URL='3']Leads[/URL]
+[CODE]''')
+                    main.append(a)
 
-if "processed_replay" not in st.session_state:
-    st.session_state.processed_replay = 0
+                else:
+                    a = j.replace("[CODE]", '''[URL='1']Moves and Teammates[/URL] | [URL='2']Combos[/url]
+[CODE]''')
+                    main.append(a)
 
-if "time" not in st.session_state:
-    st.session_state.time = 0
+            elif i >= 1:
+                k = re.sub(r'\[IMG\](.*?)\[\/IMG\]|\[B\](.*?)\[\/B\]|\[\/?CODE\]', "", j)
+                k = k.strip()
 
-if "invalid_urls" not in st.session_state:
-    st.session_state.invalid_urls = []
+                driver.get('https://pokepast.es/')
+                
+                wait.until(EC.presence_of_element_located((By.NAME, "paste")))
+                Paste = driver.find_element(By.NAME, "paste")
+                driver.execute_script("arguments[0].value = arguments[1];", Paste, k)
+                driver.execute_script("arguments[0].dispatchEvent(new Event('input', { bubbles: true }));", Paste)
 
-if "send" not in st.session_state:
-    st.session_state.send = ""
+                Submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[value='Submit Paste!']")))
+                Submit.click()
 
-if "warn" not in st.session_state:
-    st.session_state.warn = False
+                wait.until(EC.presence_of_element_located((By.TAG_NAME, "pre")))
 
-def fetch_tera(replay):
-    tera = {}
-    no_tera = 0
-    if replay.strip() == "":
-        return "", None
-    if not replay.startswith("https://replay"):
-        return "", None
-    if "gen9" not in replay:
-        return replay, None
-    retry = 0
-    while retry != 3:
-        try:
-            a = requests.get(replay + ".log")
-            a.raise_for_status()
-            break
-        except:
-            retry += 1
-    else:
-        return replay, None
-    if "|rule|Terastal Clause: You cannot Terastallize" in a.text:
-        return replay, None
-    lock = threading.Lock()
-    b = re.findall(r'(\|-terastallize\|.*: .*)', a.text)
-    if len(b) == 1:
-        with lock:
-            no_tera += 1
-    elif len(b) == 0:
-        with lock:
-            no_tera += 2
+                url.append(driver.current_url)
 
-    for i in b:
-        c = a.text.split(i)
-        d = re.findall(r'\|-terastallize\|(.*): (.*)', i)
-        e = d[0][1].split("|")
-        for j in reversed(c[0].splitlines()):
-            if re.match(rf'\|(?:switch|drag)\|{d[0][0].strip()}: {re.escape(e[0].strip())}\|([^,|]+)(?:,[^|]*)?\|', j):
-                f = re.findall(rf'\|(?:switch|drag)\|{d[0][0].strip()}: {re.escape(e[0].strip())}\|([^,|]+)(?:,[^|]*)?\|', j)
-                with lock:
-                    if f[0].strip() not in tera:
-                        tera[f[0].strip()] = []
-                    tera[f[0].strip()].append(e[1].strip())
-                break
+    driver.quit()
 
-    return tera, no_tera
+    Final = main[0].replace("-*.png",".png").replace("???", f"{key}").replace("'1'", f"'{url[0]}'").replace("'2'", f"'{url[1]}'").replace("'3'", f"'{url[2]}'")
+    bbcode.append(Final)
+    bbcode.append("")
 
-def clear():
-    st.session_state.replays = ""
-    st.session_state.final_bbcode = ""
-    st.session_state.table = []
-    st.session_state.no_tera = 0
-    st.session_state.tera = {}
-    st.session_state.sorted_tera = {}
-    st.session_state.processed_replay = 0
-    st.session_state.invalid_urls = []
-    st.session_state.send = ""
-    st.session_state.warn = False
+    return key, bbcode, rl
 
-st.text_area("**Enter Gen 9 Replay URL Here...**", key="replays", height=200)
+if st.button("Add Format", icon="➕"):
+    name_dialog()
 
-col1, col2, _ = st.columns([1, 1, 6], gap="small")
+
+for f, formats in enumerate(st.session_state.project):
+    with st.container(border=True):
+        smol1, smol2, smol3 = st.columns([1,8,1], gap="small")
+        with smol1:
+            st.text(formats)
+        with smol3:
+            if st.button("❌", key=f"delete_{f}"):
+                del st.session_state.project[formats]
+                if formats in st.session_state.all_bbcode:
+                    del st.session_state.all_bbcode[formats]
+                st.rerun()
+        links = st.text_area("Enter Replay URLs Here...", key=f"text_area_{f}", height=100)
+        st.session_state.project[formats] = links    
+
+col1, col2, col3 = st.columns([1,1,5], gap="small")
 with col1:
-    if st.button("Fetch", use_container_width=True):
-        st.session_state.no_tera = 0
-        st.session_state.tera = {}
-        st.session_state.table = []
-        st.session_state.invalid_urls = []
-        st.session_state.send = ""
-        st.session_state.processed_replay = 0
-        st.session_state.warn = False
-
-        with ThreadPoolExecutor(max_workers=50) as executor:
-            output = list(executor.map(fetch_tera, st.session_state.replays.splitlines()))
-
-            for o, p in output:
-                if o is None:
-                    continue
-                if p is None:
-                    if o != "":
-                        if o not in st.session_state.invalid_urls: 
-                            st.session_state.invalid_urls.append(o)
-                    continue
-                for key, value in o.items():
-                    if key not in st.session_state.tera:
-                        st.session_state.tera[key] = []
-                    for i in value:
-                        st.session_state.tera[key].append(i)
-                st.session_state.no_tera += p
-                st.session_state.processed_replay += 1
-
-        if st.session_state.tera == {} and st.session_state.no_tera == 0:
-            st.session_state.warn = True
+    if st.button("Generate", use_container_width=True):
+        st.session_state.processed_replays = 0
+        st.session_state.processed_formats = 0
+        if not st.session_state.project:
+            st.error("No formats available! Please add a format first.")
         else:
-            st.session_state.table = []
-            header = '''[TABLE width="100%"]
-[TR][TD width="33.3333%"]Pokemon[/TD][TD width="10%"]Count[/TD][TD width="33.3333%"]Type[/TD][/TR]'''
-            st.session_state.table.append(header)
+            with ThreadPoolExecutor(max_workers=4) as executor:
+                output = list(executor.map(replay, st.session_state.project.keys(), st.session_state.project.values()))
 
-            st.session_state.sorted_tera = {keys : values for keys, values in sorted(st.session_state.tera.items(), key = lambda item: len(item[1]), reverse=True)}
-
-            for x, y in st.session_state.sorted_tera.items():
-                counts = collections.Counter(y)
-                sorted_counts = dict(counts.most_common())
-                types = ""
-                for l, m in sorted_counts.items():
-                    types += f"{l} ({m}), "
-                types = types[:-2]
-                row = f'''[TR][TD width="33.3333%"]:{x}:{x}[/TD][TD width="10%"]{len(y)}[/TD][TD width="33.3333%"]{types}[/TD][/TR]'''
-                st.session_state.table.append(row)
-
-            closer = f'''[TR][TD width="33.3333%"]No Tera[/TD][TD width="10%"]{st.session_state.no_tera}[/TD][TD width="33.3333%"][/TD][/TR]
-[/TABLE]'''
-            st.session_state.table.append(closer)
-
-        st.session_state.final_bbcode = "\n".join(st.session_state.table)
+            for key, bbcode, rl in output:
+                st.session_state.processed_replays += rl
+                st.session_state.processed_formats += 1
+                st.session_state.all_bbcode[key] = bbcode
 
 with col2:
-    st.button("Clear", on_click=clear, use_container_width=True)
+    if st.button("Clear All", use_container_width=True):
+        st.session_state.project.clear()
+        st.session_state.all_bbcode.clear()
+        st.session_state.final.clear()
+        st.session_state.processed_replays = 0
+        st.session_state.processed_formats = 0
+        st.rerun()
 
-if st.session_state.warn == True:
-    st.warning("No Replays Found! Please Enter Atleast One Valid Gen 9 Link!")
+for i in st.session_state.project.keys():
+    if i in st.session_state.all_bbcode:
+        st.session_state.final.append("\n".join(st.session_state.all_bbcode[i]))
 
-if st.session_state.processed_replay != 0:
-    st.caption(f"Processed {st.session_state.processed_replay} replays.")
-st.caption("BB Code:")
-st.code(st.session_state.final_bbcode, language=None, height=300)
-
-if st.session_state.invalid_urls != []:
-    st.session_state.send = ""
-    for d in st.session_state.invalid_urls:
-        st.session_state.send += f"- {d}\n\n"
-    st.warning(f'''Invalid Replays Found: \n\n
-{st.session_state.send}''')
+if st.session_state.final:
+    done = "\n".join(st.session_state.final)
+    st.caption(f"Processed {st.session_state.processed_formats} formats and {st.session_state.processed_replays} replays.")
+    end = time.time()
+    st.caption(f"Time taken: {end - start} seconds.")
+    st.caption("BB Code:")
+    st.code(done, language=None, height=300)
